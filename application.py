97c9f4385d6c806f5cb59ade9742ec2698d444b2
Harrison Zoccoli <harrisonzoccoli@gmail.com>
2025-03-17 19:25:22 -0400

    Updated application.py with WSGI object and modified startup command

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
 # This is the entry point for Azure
 from app import app, socketio
 
+# Create the WSGI application object
+application = app
+
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8000) 
+    socketio.run(app, host='0.0.0.0', port=8000)
