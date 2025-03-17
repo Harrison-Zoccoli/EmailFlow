e0ec24a3abc62757c699ffe75d549cb3fa2a5e4b
Harrison Zoccoli <harrisonzoccoli@gmail.com>
2025-03-17 19:46:04 -0400

    Clean start with minimal files

--- startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/site/wwwroot
-pip install --upgrade pip
-pip install -r requirements.txt
-gunicorn --bind=0.0.0.0 --timeout 600 --worker-class eventlet --log-level debug application:app 
